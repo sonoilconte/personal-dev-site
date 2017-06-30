5b1beb7372c9166d2f86e4a569c1578bd0183eff
sonoilconte <shammersinger@yahoo.com>
2017-06-30 09:04:49 -0700

    draft complete

---
+++ assets/js/fade.js
@@ -0,0 +1,59 @@
+//fades in and out the wrapping div as you switch sections
+
+// function fadeHide(){
+// $('#wrapper').fadeOut(1000);
+// $('#work').hide();
+// $('#about').hide();
+// $('#contact').hide();
+// }
+
+$(document).ready(function(){
+
+	console.log("fade js working");
+
+//start with only work section showing 
+
+	$('#about').hide();
+	$('#contact').hide();
+
+//get the section tags from the DOM
+	var work = $('#work').html();
+	var about = $('#about').html();
+	var contact = $('#contact').html();
+
+console.log(work, about, contact);
+
+	$('#nav-work').on('click', function(){
+
+			$('#wrapper').fadeOut(1000, function(){
+				$('#wrapper').html(work).fadeIn(1000);
+				}
+			);
+		}
+	);
+
+	$('#nav-about').on('click', function(){
+		
+			$('#wrapper').fadeOut(1000, function(){
+					$('#wrapper').html(about).fadeIn(1000);
+					}
+			);
+		}
+	);
+
+	$('#nav-contact').on('click', function(){
+			
+			$('#wrapper').fadeOut(1000, function(){
+				$('#wrapper').html(contact).fadeIn(1000);
+				}
+			);
+		}
+	);
+
+});
+
+
+
+
+
+
