48f2af8b1884ae4e0942ea3da186b765b1062c54
sonoilconte <shammersinger@yahoo.com>
2019-05-23 16:42:14 -0700

    Refactor JS for DRYness and better use of styles

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -1,59 +1,31 @@
-// TODO: Current section should be highlighted
+$(document).ready(function() {
 
-$(document).ready(function(){
+	// Start with only work section showing
+	$('#nav-work').addClass('current-btn');
+	$('#work').addClass('display');
 
-//start with only work section showing
+	const elements = document.getElementsByClassName('nav-btn');
 
-	$('#about').hide();
-	$('#contact').hide();
-
-//get the section tags from the DOM
-	let work = $('#work').html();
-	let about = $('#about').html();
-	let contact = $('#contact').html();
-
-	$('#nav-work').on('click', (e) => {
-		e.preventDefault();
-		$('#nav-work').addClass('current-btn');
-		$('#nav-about').removeClass('current-btn');
-		$('#nav-contact').removeClass('current-btn');
-
-		$('#wrapper').fadeOut(500, () => {
-			$('#wrapper').html(work).fadeIn(500);
-		});
-	});
-
-	$('#nav-about').on('click', (e) => {
-		e.preventDefault();
-		$('#nav-about').addClass('current-btn');
-		$('#nav-work').removeClass('current-btn');
-		$('#nav-contact').removeClass('current-btn');
-
-		$('#wrapper').fadeOut(500, () => {
-			$('#wrapper').html(about).fadeIn(500);
-		});
-	});
-
-	$('#nav-contact').on('click', (e) => {
-		e.preventDefault();
-		$('#nav-contact').addClass('current-btn');
-		$('#nav-work').removeClass('current-btn');
-		$('#nav-about').removeClass('current-btn');
-
-		$('#wrapper').fadeOut(500, () => {
-			$('#wrapper').html(contact).fadeIn(500);
+	Array.from(elements).forEach(el => {
+		el.addEventListener('click', e => {
+			e.preventDefault;
+			$('li').removeClass('current-btn');
+			$(`#${e.target.id}`).addClass('current-btn');
+			$('#wrapper').fadeOut(500, () => {
+				$('section').removeClass('display');
+				const sectionId = e.target.id.slice(4);
+				$(`#${sectionId}`).addClass('display');
+				$('#wrapper').fadeIn(500);
+			});
 		});
 	});
 
 	// Watch for scroll for fixed nav bar
-
 	$(window).scroll(() => {
-		console.log($(window).scrollTop());
-		if ($(window).scrollTop() > 300){
+		if ($(window).scrollTop() > 320){
 			$('nav').addClass('fixed-nav');
 			$('#wrapper').css('margin-top', '80px');
-		}
-		if ($(window).scrollTop() < 300){
+		} else {
 			$('nav').removeClass('fixed-nav');
 			$('#wrapper').css('margin-top', '0');
 		}
