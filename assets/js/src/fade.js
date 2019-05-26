f4656ffda3834a843bb7fea27fadffdc4c3c3cc2
sonoilconte <shammersinger@yahoo.com>
2019-05-25 21:39:53 -0700

    Install eslint in project as dev dependency Lint and make corrections in fade.js

--- assets/js/src/fade.js
+++ assets/js/src/fade.js
@@ -5,31 +5,31 @@ document.getElementById('work').classList.add('display');
 
 const navElements = document.getElementsByClassName('nav-btn');
 
-Array.from(navElements).forEach(el => {
-	el.addEventListener('click', e => {
-		e.preventDefault();
-		Array.from(document.getElementsByTagName('li'))
-			.forEach(el => el.classList.remove('current-btn'));
-		document.getElementById(e.target.id).classList.add('current-btn');
-		$('#fade-wrapper').fadeOut(500, () => {
-			Array.from(document.getElementsByTagName('section'))
-				.forEach(el => el.classList.remove('display'))
-			document.getElementById(e.target.id.slice(4)).classList.add('display');
-			if (window.scrollY > mobileOffset) {
-				window.scrollTo(0, mobileOffset);
-			}
-			$('#fade-wrapper').fadeIn(500);
-		});
-	});
+Array.from(navElements).forEach((navElement) => {
+  navElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    Array.from(document.getElementsByTagName('li'))
+      .forEach(el => el.classList.remove('current-btn'));
+    document.getElementById(e.target.id).classList.add('current-btn');
+    $('#fade-wrapper').fadeOut(500, () => {
+      Array.from(document.getElementsByTagName('section'))
+        .forEach(el => el.classList.remove('display'));
+      document.getElementById(e.target.id.slice(4)).classList.add('display');
+      if (window.scrollY > mobileOffset) {
+        window.scrollTo(0, mobileOffset);
+      }
+      $('#fade-wrapper').fadeIn(500);
+    });
+  });
 });
 
 // Watch for scroll for fixed nav bar
 window.addEventListener('scroll', () => {
-	if (window.scrollY > mobileOffset) {
-		document.getElementsByTagName('nav')[0].classList.add('fixed-nav');
-		document.getElementById('wrapper').classList.add('wrapper-nav-offset');
-	} else {
-		document.getElementsByTagName('nav')[0].classList.remove('fixed-nav');
-		document.getElementById('wrapper').classList.remove('wrapper-nav-offset');
-	}
+  if (window.scrollY > mobileOffset) {
+    document.getElementsByTagName('nav')[0].classList.add('fixed-nav');
+    document.getElementById('wrapper').classList.add('wrapper-nav-offset');
+  } else {
+    document.getElementsByTagName('nav')[0].classList.remove('fixed-nav');
+    document.getElementById('wrapper').classList.remove('wrapper-nav-offset');
+  }
 });
