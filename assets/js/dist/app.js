70dc3529e510d7e224c85e57afecefbec9993004
sonoilconte <shammersinger@yahoo.com>
2019-05-28 22:10:11 -0700

    Delete uneeded files and run gulp/babel

--- assets/js/dist/app.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-// sanity check
-console.log("app.js linked.");
--- assets/js/dist/fade.js
+++ assets/js/dist/fade.js
@@ -1,40 +1,38 @@
 "use strict";
 
-$(document).ready(function () {
-  var mobileOffset = 270; // Start with only work section showing
+var mobileOffset = 270; // Start with nav-work button highlighted and only work section showing
 
-  $('#nav-work').removeClass('default-btn').addClass('current-btn');
-  $('#work').addClass('display');
-  var elements = document.getElementsByClassName('nav-btn');
-  Array.from(elements).forEach(function (el) {
-    el.addEventListener('click', function (e) {
-      e.preventDefault();
-      $('li').removeClass('current-btn');
-      $("#".concat(e.target.id)).addClass('current-btn');
-      $('#fade-wrapper').fadeOut(500, function () {
-        $('section').removeClass('display');
-        var sectionId = e.target.id.slice(4);
-        $("#".concat(sectionId)).addClass('display');
-
-        if ($(window).scrollTop() > mobileOffset) {
-          console.log('reset window');
-          window.scrollTo(0, mobileOffset);
-        }
-
-        $('#fade-wrapper').fadeIn(500, function () {
-          console.log("faded in");
-        });
-      });
+document.getElementById('nav-work').classList.add('current-btn');
+document.getElementById('work').classList.add('display');
+var navElements = document.getElementsByClassName('nav-btn');
+Array.from(navElements).forEach(function (navElement) {
+  navElement.addEventListener('click', function (e) {
+    e.preventDefault();
+    Array.from(document.getElementsByTagName('li')).forEach(function (el) {
+      return el.classList.remove('current-btn');
     });
-  }); // Watch for scroll for fixed nav bar
+    document.getElementById(e.target.id).classList.add('current-btn');
+    $('#fade-wrapper').fadeOut(500, function () {
+      Array.from(document.getElementsByTagName('section')).forEach(function (el) {
+        return el.classList.remove('display');
+      });
+      document.getElementById(e.target.id.slice(4)).classList.add('display');
 
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > mobileOffset) {
-      $('nav').addClass('fixed-nav');
-      $('#wrapper').css('margin-top', '80px');
-    } else {
-      $('nav').removeClass('fixed-nav');
-      $('#wrapper').css('margin-top', '0');
-    }
+      if (window.scrollY > mobileOffset) {
+        window.scrollTo(0, mobileOffset);
+      }
+
+      $('#fade-wrapper').fadeIn(500);
+    });
   });
+}); // Watch for scroll for fixed nav bar
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > mobileOffset) {
+    document.getElementsByTagName('nav')[0].classList.add('fixed-nav');
+    document.getElementsByClassName('wrapper')[0].classList.add('wrapper-nav-offset');
+  } else {
+    document.getElementsByTagName('nav')[0].classList.remove('fixed-nav');
+    document.getElementsByClassName('wrapper')[0].classList.remove('wrapper-nav-offset');
+  }
 });
