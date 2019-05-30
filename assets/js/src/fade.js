d0bd44c029eb95e3ebf7e0b48f29510e72350a1b
sonoilconte <shammersinger@yahoo.com>
2019-05-29 21:51:52 -0700

    Use getComputedStyle() to find transition duration from css

--- assets/js/src/fade.js
+++ assets/js/src/fade.js
@@ -4,21 +4,26 @@ const sectionElements = Array.from(document.getElementsByTagName('section'));
 // Start with nav-work button highlighted and only work section showing
 navElements[0].classList.add('current-btn');
 sectionElements[0].classList.add('display');
+const fadeElement = document.getElementById('fade-wrapper');
 
 navElements.forEach((navElement) => {
   navElement.addEventListener('click', (e) => {
     e.preventDefault();
     navElements.forEach((el) => { el.classList.remove('current-btn') });
     e.target.classList.add('current-btn');
-    $('#fade-wrapper').fadeOut(500, () => {
+    fadeElement.classList.add('hidden');
+    const fadeDuration = getComputedStyle(fadeElement).transitionDuration;
+    const durationMs = parseFloat(fadeDuration.match(/\d+.\d+/))*1000;
+    window.setTimeout(() => {
       sectionElements.forEach((el) => { el.classList.remove('display') });
       sectionElements.find(el => el.id === e.target.dataset.section)
         .classList.add('display');
       if (window.scrollY > mobileOffset) {
         window.scrollTo(0, mobileOffset);
       }
-      $('#fade-wrapper').fadeIn(500);
-    });
+      fadeElement.classList.remove('hidden');
+      fadeElement.classList.add('visible');
+    }, durationMs);
   });
 });
 
