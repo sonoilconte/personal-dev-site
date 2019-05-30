6190b512439a7edd30eaef0780afbd6f70952675
sonoilconte <shammersinger@yahoo.com>
2019-05-30 16:09:25 -0700

    Add function getActiveElements to keep code DRYer

--- assets/js/src/fade.js
+++ assets/js/src/fade.js
@@ -3,24 +3,33 @@ const headerGeometry = headerElement.getBoundingClientRect();
 const stickyNavOffset = headerGeometry.height + window.scrollY + headerGeometry.y;
 const navElements = Array.from(document.getElementsByClassName('nav-btn'));
 const sectionElements = Array.from(document.getElementsByTagName('section'));
-// Start with 1st button highlighted and only 1st section showing
-navElements[0].classList.add('current-btn');
-sectionElements[0].classList.add('display');
 const fadeElement = document.getElementById('fade-wrapper');
+
+function getActiveElements(locationHash) {
+  if (locationHash && locationHash !== '') {
+    const navElement = navElements.find(el => el.parentElement.getAttribute('href') == locationHash);
+    const sectionElement = sectionElements.find(el => el.id == locationHash.substring(1));
+    return [navElement, sectionElement];
+  } else {
+    return [navElements[0], sectionElements[0]];
+  }
+}
+// Page load could be index.html, or index.html#contact etc.
+const activeElements = getActiveElements(location.hash);
+activeElements[0].classList.add('current-btn');
+activeElements[1].classList.add('display');
 
 window.addEventListener('hashchange', (e) => {
   e.preventDefault();
-  console.log('hash change to', location.hash);
   navElements.forEach((el) => { el.classList.remove('current-btn') });
-  navElements.find(el => el.parentElement.getAttribute('href') === location.hash)
-    .classList.add('current-btn');
+  const activeElements = getActiveElements(location.hash);
+  activeElements[0].classList.add('current-btn');
   fadeElement.classList.add('hidden');
   const fadeDuration = getComputedStyle(fadeElement).transitionDuration;
   const durationMs = parseFloat(fadeDuration.match(/\d+.\d+/))*1000;
   window.setTimeout(() => {
     sectionElements.forEach((el) => { el.classList.remove('display') });
-    sectionElements.find(el => el.id === location.hash.substring(1))
-    .classList.add('display');
+    activeElements[1].classList.add('display');
     if (window.scrollY > stickyNavOffset) {
       window.scrollTo(0, stickyNavOffset);
     }
@@ -30,7 +39,6 @@ window.addEventListener('hashchange', (e) => {
 });
 
 // Watch for scroll for fixed nav bar
-
 let navIsSticky = false;
 
 window.addEventListener('scroll', () => {
