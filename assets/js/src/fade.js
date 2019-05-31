82eeabababeef65022a89150de974310fabad37c
sonoilconte <shammersinger@yahoo.com>
2019-05-30 22:32:04 -0700

    Lint, add new method selectWithLocation for DRYness Transpile

--- assets/js/src/fade.js
+++ assets/js/src/fade.js
@@ -5,37 +5,39 @@ const navElements = Array.from(document.getElementsByClassName('nav-btn'));
 const sectionElements = Array.from(document.getElementsByTagName('section'));
 const fadeElement = document.getElementById('fade-wrapper');
 
+function selectWithLocation(el, locationHash) {
+  return el.dataset.section === locationHash.substring(1);
+}
+
 function getActiveElements(locationHash) {
   if (locationHash && locationHash !== '') {
-    const navElement = navElements.find(el => el.parentElement.getAttribute('href') == locationHash);
-    const sectionElement = sectionElements.find(el => el.id == locationHash.substring(1));
+    const navElement = navElements.find(el => selectWithLocation(el, locationHash));
+    const sectionElement = sectionElements.find(el => selectWithLocation(el, locationHash));
     return [navElement, sectionElement];
-  } else {
-    return [navElements[0], sectionElements[0]];
   }
+  return [navElements[0], sectionElements[0]];
 }
+
 // Page load could be index.html, or index.html#contact etc.
-const activeElements = getActiveElements(location.hash);
+const activeElements = getActiveElements(window.location.hash);
 activeElements[0].classList.add('current-btn');
 activeElements[1].classList.add('display');
 
-// Make sure a direct link to a #contact etc. does not scroll down
-// Is this too hacky?
-if (location.hash && location.hash != '') {
-  setTimeout(() => { window.scrollTo(0, 0) }, 1);
-}
-
 window.addEventListener('hashchange', (e) => {
   e.preventDefault();
-  navElements.forEach((el) => { el.classList.remove('current-btn') });
-  const activeElements = getActiveElements(location.hash);
-  activeElements[0].classList.add('current-btn');
+  navElements.forEach((el) => {
+    el.classList.remove('current-btn');
+  });
+  const elements = getActiveElements(window.location.hash);
+  elements[0].classList.add('current-btn');
   fadeElement.classList.add('hidden');
   const fadeDuration = getComputedStyle(fadeElement).transitionDuration;
-  const durationMs = parseFloat(fadeDuration.match(/\d+.\d+/))*1000;
+  const durationMs = parseFloat(fadeDuration.match(/\d+.\d+/)) * 1000;
   window.setTimeout(() => {
-    sectionElements.forEach((el) => { el.classList.remove('display') });
-    activeElements[1].classList.add('display');
+    sectionElements.forEach((el) => {
+      el.classList.remove('display');
+    });
+    elements[1].classList.add('display');
     if (window.scrollY > stickyNavOffset) {
       window.scrollTo(0, stickyNavOffset);
     }
@@ -49,7 +51,7 @@ let navIsSticky = false;
 
 window.addEventListener('scroll', () => {
   const navShouldBeSticky = window.scrollY > stickyNavOffset;
-  if (navShouldBeSticky == navIsSticky) {
+  if (navShouldBeSticky === navIsSticky) {
     return;
   }
   document.body.classList.toggle('sticky', navShouldBeSticky);
