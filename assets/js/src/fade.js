81ec39f891ee844eb795dd544813a508dcdcb7a5
sonoilconte <shammersinger@yahoo.com>
2019-06-03 11:16:19 -0700

    Refactor with function getActiveElement the works for navs or sections

--- assets/js/src/fade.js
+++ assets/js/src/fade.js
@@ -5,39 +5,31 @@ const navElements = Array.from(document.getElementsByClassName('nav-btn'));
 const sectionElements = Array.from(document.getElementsByTagName('section'));
 const fadeElement = document.getElementById('fade-wrapper');
 
-function selectWithLocation(el, locationHash) {
-  return el.dataset.section === locationHash.substring(1);
-}
-
-function getActiveElements(locationHash) {
-  if (locationHash && locationHash !== '') {
-    const navElement = navElements.find(el => selectWithLocation(el, locationHash));
-    const sectionElement = sectionElements.find(el => selectWithLocation(el, locationHash));
-    return [navElement, sectionElement];
+function getActiveElement(locationHash, elementsArray) {
+  if (locationHash) {
+    return elementsArray.find(el => el.dataset.section === locationHash.substring(1));
   }
-  return [navElements[0], sectionElements[0]];
+  return elementsArray[0];
 }
 
 // Page load could be index.html, or index.html#contact etc.
-const activeElements = getActiveElements(window.location.hash);
-activeElements[0].classList.add('current-btn');
-activeElements[1].classList.add('display');
+getActiveElement(window.location.hash, navElements).classList.add('current-btn');
+getActiveElement(window.location.hash, sectionElements).classList.add('display');
 
 window.addEventListener('hashchange', (e) => {
-  e.preventDefault();
   navElements.forEach((el) => {
     el.classList.remove('current-btn');
   });
-  const elements = getActiveElements(window.location.hash);
-  elements[0].classList.add('current-btn');
+  getActiveElement(window.location.hash, navElements).classList.add('current-btn');
   fadeElement.classList.add('hidden');
+  // Get transitionDuration from CSS and convert seconds string to milliseconds
   const fadeDuration = getComputedStyle(fadeElement).transitionDuration;
   const durationMs = parseFloat(fadeDuration.match(/\d+.\d+/)) * 1000;
   window.setTimeout(() => {
     sectionElements.forEach((el) => {
       el.classList.remove('display');
     });
-    elements[1].classList.add('display');
+    getActiveElement(window.location.hash, sectionElements).classList.add('display');
     if (window.scrollY > stickyNavOffset) {
       window.scrollTo(0, stickyNavOffset);
     }
