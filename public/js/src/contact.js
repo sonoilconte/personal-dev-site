6a44cd5400272c2e5415d08ad0b8efb6d4764235
sonoilconte <shammersinger@yahoo.com>
2019-06-06 16:17:13 -0700

    Handling for success and failure of contact form submit

--- public/js/src/contact.js
+++ public/js/src/contact.js
@@ -1,9 +1,35 @@
 const form = document.querySelector('form');
+const formElements = Array.from(form);
+const resultMessageDiv = document.getElementById('result-message');
+
+function onSubmitMessage() {
+  window.setTimeout(() => {
+    form.classList.toggle('default-no-display', true);
+    resultMessageDiv.classList.toggle('display', true);
+    formElements.forEach(el => {
+      if (el.value) {
+        el.value = "";
+      }
+    });
+  }, 2000);
+}
+
+function readyFormDisplay() {
+  form.classList.toggle('default-no-display', false);
+  resultMessageDiv.classList.toggle('display', false);
+}
+
+function onSuccess() {
+  resultMessageDiv.insertAdjacentHTML('beforeend', '&#9989');
+}
+
+function onFailure() {
+  resultMessageDiv.innerHTML = 'There was problem sending your info. Please contact me on <a href="https://www.linkedin.com/in/sepphammer/">LinkedIn</a>';
+}
 
 form.onsubmit = (e) => {
   e.preventDefault();
   const formData = {};
-  const formElements = Array.from(form);
   formElements.map((el) => {
     if (el.name && el.value) {
       return formData[el.name] = el.value;
@@ -20,6 +46,14 @@ form.onsubmit = (e) => {
   };
 
   axios(options)
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+  .then(res => {
+    console.log(res);
+    onSuccess();
+  })
+  .catch(err => {
+    console.log(err);
+    onFailure();
+  });
+
+  onSubmitMessage();
 }
